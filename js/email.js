763de69ad83bee0d0dfb80a8801c120427f99e0d
Jake Flynn <jflynn@reapit.com>
2017-06-13 16:55:44 +0100

    adding captions

--- js/email.js
+++ js/email.js
@@ -72,8 +72,21 @@ $(document).ready(function(){
 		var modal = $('<div class="modal"></div>');
 		image.click(function(){
 			var img = $('<img src="img/' + this.id + '.png">');
-			$(modal).append(img);
+			var caption = $('<p class="caption ' + this.id + '"></p>'); 
+			$(modal).append(img, caption);
 			$('body').append(modal);
+			$('.appraisal01').html(
+				'This is the screen that is presented after a user clicks the "Book a Valuation" button.<br/><br/>They must fill in their personal details before seeing the list of available appointments.');
+			$('.appraisal02').html(
+				'Once there is a valid post code match against your offices, the user is presented with a diary screen where unavailable times are not visible.<br/><br/>National holidays and company event days can be booked out by default to ensure appointments are not made on these days.');
+			$('.appraisal03').html(
+				'As soon as an appointment slot has been selected, the user will be requested for further details.');
+			$('.appraisal04').html(
+				'Here is an example of how a successful appointment booking looks.');
+			$('.appraisal05').html(
+				'The email the client receieves as confirmation.');
+			$('.appraisal06').html(
+				'The SMS message the client receieves as confirmation.');
 			if($(modal).css('display') == 'none'){
 				$(modal).css('display', 'block');
 			}
